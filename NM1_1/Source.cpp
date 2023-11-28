572fc34834c857df54811de66823b186f236d57f
rubyRain0 <den015326@gmail.com>
2023-11-28 19:29:56 +0300

    acc testing

--- NM1_1/Source.cpp
+++ NM1_1/Source.cpp
@@ -4,8 +4,9 @@
 #include <vector>
 #include <sstream>
 #include <random>
+#include <cmath>
 
-const double E = 0.0000001;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    int k = 1000000;
 
 double generateRandomNumber(double range_min, double range_max) {
     static std::random_device rd;
@@ -22,7 +23,7 @@ double roundError(double error) {
     return roundedError;
 }
 
-void printArrays(int N, long double** a, long double** b, long double** c, long double* p, long double* q, long double* f)
+void printArrays(int N, long double** a, long double** b, long double** c, long double* p, long double* q, long double* f, long double* ft)
 {
     std::cout << "p: ";
     for (int i = 0; i < N; ++i) {
@@ -77,6 +78,12 @@ void printArrays(int N, long double** a, long double** b, long double** c, long 
         std::cout << f[i] << " ";
     }
     std::cout << std::endl;
+
+    std::cout << "ft: ";
+    for (int i = 0; i < N; ++i) {
+        std::cout << ft[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
 void inputMatrixFromFile(int N, long double** a, 
@@ -88,9 +95,10 @@ void inputMatrixFromFile(int N, long double** a,
     long double* f,
     long double* ft,
     long double* p, 
-    long double* q)
+    long double* q,
+    std::string filename)
 {
-    std::ifstream file("data.txt");
+    std::ifstream file(filename);
     if (!file) {
         std::cout << "Error opening the file." << std::endl;
         return;
@@ -99,10 +107,12 @@ void inputMatrixFromFile(int N, long double** a,
     //Not Overlapping Elements:
     for (size_t i = 0; i < N; ++i)
     {
+        long double sum = 0;
         for (size_t j = 0; j < N; ++j)
         {   
-            file >> el;
-            if (el != 0 || j==0 || j == N-1 || i+j == N-2)
+            file >> el;  
+            sum += el;
+            if (el != 0 || j==0 || j == N-1 || i+j == N-2 || i+j == N)
             {   
                 if (j == 0)
                     p[i] = el;
@@ -123,6 +133,7 @@ void inputMatrixFromFile(int N, long double** a,
                 }
             }
         }
+        ft[i] = sum;
     }
     //Overlapping Elements:
     b[0] = &q[0];
@@ -152,7 +163,7 @@ void inputMatrixFromFile(int N, long double** a,
     }
 }
 
-void WriteMatrixToFile(int N, long double** a, long double** b, long double** c, long double* p, long double* q, long double* f, std::string filename)
+void WriteMatrixToFile(int N, long double** a, long double** b, long double** c, long double* p, long double* q, long double* f, long double* ft, std::string filename)
 {
     std::ofstream ofile(filename);
     for (size_t i = 0; i < N; ++i)
@@ -182,49 +193,67 @@ void WriteMatrixToFile(int N, long double** a, long double** b, long double** c,
     {
         ofile << f[i] << '\n';
     }
+
+    ofile << "\n\n";
+    for (size_t i = 0; i < N; i++)
+    {
+        ofile << ft[i] << '\n';
+    }
 }
 
-bool isCorrectToBeSolved(int N, long double* f, long double* p)
-{
-    if ((f[N - 1] > E || f[N - 1] < -E) && p[N - 1] == 0)
-        return false;
-    return true;
-}
-
-void solution(int N, long double** a, long double** b, long double** c, long double* p, long double* q, long double* f, long double* ft, std::string filename)
+bool solution(int N, long double** a, long double** b, long double** c, long double* p, long double* q, long double* f, long double* ft, std::string filename)
 {   
+    bool solution = true;
     //Step1: Clear bottom diag.
     for (size_t i = 1; i < N-2; ++i)
     {
-        p[i] /= *b[i];
-        *c[i] /= *b[i];
-        q[i] /= *b[i];
-        f[i] /= *b[i];
-        *b[i] = 1;
+        if (*b[i] == 0)
+            solution = false;
+        if (*b[i] != 0)
+        {
+            p[i] /= *b[i];
+            *c[i] /= *b[i];
+            q[i] /= *b[i];
+            ft[i] /= *b[i];
+            f[i] /= *b[i];
+            *b[i] = 1;
 
-        p[i + 1] += p[i] * -*a[i];
-        *b[i + 1] += *c[i] * -*a[i];
-        q[i + 1] += q[i] * -*a[i];
-        f[i + 1] += f[i] * -*a[i];
+            p[i + 1] += p[i] * -*a[i];
+            *b[i + 1] += *c[i] * -*a[i];
+            q[i + 1] += q[i] * -*a[i];
+            f[i + 1] += f[i] * -*a[i];
+            ft[i + 1] += ft[i] * -*a[i];
 
-        *a[i] = 0; //?    
+            *a[i] = 0; //?    
+        }
     }
 
     //N-2 before-last row
+    if (*b[N - 2] == 0)
+        solution = false;
+    if (*b[N-2] != 0) {
+        p[N - 2] /= *b[N - 2];
+        q[N - 2] /= *b[N - 2];
+        f[N - 2] /= *b[N - 2];
+        ft[N - 2] /= *b[N - 2];
+        *b[N - 2] = 1;
 
-    p[N - 2] /= *b[N - 2];
-    q[N - 2] /= *b[N - 2];
-    f[N - 2] /= *b[N - 2];
-    *b[N - 2] = 1;
-
-    p[N - 1] += p[N - 2] * -*a[N-2];
-    q[N - 1] += q[N - 2] * -*a[N-2];
-    f[N - 1] += f[N - 2] * -*a[N-2];
-    *a[N - 2] = 0;
+        p[N - 1] += p[N - 2] * -*a[N - 2];
+        q[N - 1] += q[N - 2] * -*a[N - 2];
+        f[N - 1] += f[N - 2] * -*a[N - 2];
+        ft[N - 1] += ft[N - 2] * -*a[N - 2];
+        *a[N - 2] = 0;
+    }
     
-    q[N - 1] /= *b[N - 1];
-    f[N - 1] /= *b[N - 1];
-    p[N - 1] /= *b[N - 1];
+    if (*b[N - 1] == 0)
+        solution = false;
+    if (*b[N-1]!=0)
+    {
+        q[N - 1] /= *b[N - 1];
+        f[N - 1] /= *b[N - 1];
+        ft[N - 1] /= *b[N - 1];
+        p[N - 1] /= *b[N - 1];
+    }
 
     //Step2: (Clear first col)
     for (int i = N-2; i >= 0; i--)
@@ -233,6 +262,7 @@ void solution(int N, long double** a, long double** b, long double** c, long dou
         {
             q[i] += q[N - 1] * -p[i];
             f[i] += f[N - 1] * -p[i];
+            ft[i] += ft[N - 1] * -p[i];
             p[i] += p[N - 1] * -p[i];
          
         }
@@ -243,37 +273,61 @@ void solution(int N, long double** a, long double** b, long double** c, long dou
     {
        q[i - 1] += q[i] * -*c[i - 1];
        f[i - 1] += f[i] * -*c[i - 1];
+       ft[i - 1] += ft[i] * -*c[i - 1];
        *c[i - 1] += *b[i] * -*c[i - 1];
     }
-    f[0] /= *b[0];
-    q[0] = 1;
-
-    //Step4: (Clear last col)
-    for (size_t i = 0; i < N-1; ++i)
+    for (size_t i = 0; i < N; i++)
     {
-        if (q[i + 1] != 0)
-        {
-            f[i + 1] += f[0] * -q[i + 1];
-            q[i + 1] += q[0] * -q[i + 1];
-        }
+        if (*b[i] == 0)
+            solution = false;
     }
-    WriteMatrixToFile(N, a, b, c, p, q, f, filename);
-    //printArrays(N, a, b, c, p, q, f);
-    long double* x = new long double[N];
-    if (isCorrectToBeSolved)
+    if (solution && *b[0] != 0)
     {
+        f[0] /= *b[0];
+        ft[0] /= *b[0];
+        q[0] = 1;
+
+        //Step4: (Clear last col)
+        for (size_t i = 0; i < N - 1; ++i)
+        {
+            if (q[i + 1] != 0)
+            {
+                f[i + 1] += f[0] * -q[i + 1];
+                ft[i + 1] += ft[0] * -q[i + 1];
+                q[i + 1] += q[0] * -q[i + 1];
+            }
+        }
+        WriteMatrixToFile(N, a, b, c, p, q, f, ft, filename);
+        //printArrays(N, a, b, c, p, q, f);
+        long double* x = new long double[N];
         std::cout << "Solution vector: \n";
         for (size_t i = 0; i < N; ++i)
         {
             x[i] = f[i];
             std::cout << x[i] << std::endl;
         }
+        long double acc = 0;
+        long double delt = 0;
+        for (int i = 0; i < N; i++)
+        {
+            acc = std::max(abs(ft[i]-1.0), acc);
+            delt = std::max(abs(ft[i] - 1.0) / f[i], delt);
+        }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                if (acc < 1 && delt < 1 && x[0]<10000000000)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                {
+        std::cout << "Accuracy of solution: " << std::endl;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             delt *= k;
+        std::cout << acc << " | " << delt << std::endl;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+            
+        else std::cout << "No solution.\n";
     }
-
+    else std::cout << "No solution.\n";
+    return solution;
 }
 
 void generateMatrixTestAccuracy(int N, int minValue, int maxValue)
-{
+{   
+    std::cout << "Test Case of size " << N << ". Value range: (" << minValue << "; " << maxValue << ")" << std::endl;
     long double** a = new long double*[N - 1]; //under
     long double* a_t = new long double[N - 1]; //under
 
@@ -288,20 +342,29 @@ void generateMatrixTestAccuracy(int N, int minValue, int maxValue)
     long double* p = new long double[N];
     long double* q = new long double[N];
 
+    std::fstream file("randomData.txt");
     for (size_t i = 0; i < N; ++i)
     {
-        *b[i] = generateRandomNumber(minValue, maxValue);
-        p[i] = generateRandomNumber(minValue, maxValue);
-        q[i] = generateRandomNumber(minValue, maxValue);
-        f[i] = generateRandomNumber(minValue, maxValue);
-
+        for (size_t j = 0; j < N; ++j)
+        {
+            if (i + j == N - 1 || j == 0 || j == N - 1 || i + j == N - 2 || i + j == N)
+            {
+                file << generateRandomNumber(minValue, maxValue) << "\t\t";
+            }
+            else file << 0 << "\t\t";
+        }
+        file << "\n";
     }
-    for (size_t i = 0; i < N-1; ++i)
+    file << "\n";
+    for (size_t i = 0; i < N; i++)
     {
-        *a[i] = generateRandomNumber(minValue, maxValue);
-        *c[i] = generateRandomNumber(minValue, maxValue);
+        file << generateRandomNumber(minValue, maxValue) << "\n";
     }
-    WriteMatrixToFile(N, a, b, c, p, q, f, "randomSystemTest.txt");
+
+
+
+    inputMatrixFromFile(N, a, a_t, b, b_t, c, c_t, f, ft, p, q, "randomData.txt");
+    //printArrays(N, a, b, c, p, q, f, ft);
     solution(N, a, b, c, p, q, f, ft, "randomSystemResult.txt");
 }
 
@@ -327,10 +390,10 @@ int main() {
     long double* p = new long double[N];
     long double* q = new long double[N];
   
-    inputMatrixFromFile(N, a, a_t, b, b_t, c, c_t, f, ft, p, q);
-    printArrays(N, a, b, c, p, q, f);
+    inputMatrixFromFile(N, a, a_t, b, b_t, c, c_t, f, ft, p, q, "data.txt");
+    //printArrays(N, a, b, c, p, q, f, ft);
     solution(N, a, b, c, p, q, f, ft, "resultSystem.txt");
-    //generateMatrixTestAccuracy(100, -10, 10);
+    generateMatrixTestAccuracy(1000, -100, 100);
 
     return 0;
 }
